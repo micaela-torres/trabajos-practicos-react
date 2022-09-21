d7c1ff2e10b4d24ea580ccc4f3d15fba5e75b33b
micaela-torres <torresmicaela2111@gmail.com>
2022-09-21 16:29:09 -0300

    descripcion

--- src/Componentes/ItemList.js
+++ src/Componentes/ItemList.js
@@ -1,51 +1,21 @@
 import Item from "./Item";
-import productos from "../productos.json";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useState } from "react";
 
-
-
-const ItemList = () => {
-    const [data, setData] = useState([]);
-
-    const { marcaid } = useParams();
-    useEffect(() => {
-        const data = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(productos);
-            })
-        });
-        if (marcaid) {
-            data.then(res => setData(res.filter(Algabo => Algabo.marca === marcaid)));
-        } else {
-            data.then(res => setData(res));
-        }
-
-    }, [marcaid]);
-
+const ItemList = ({ props }) => {
     return (
         <div>
             {
-                productos.map((productos) => {
-                    return (
-                        <>
-                            <Item
-                                key={productos.codigo}
-                                nombre={productos.producto}
-                                descripcion={productos.descripcion}
-                                marca={productos.marca}
-                                precio={productos.precio}
-                            />
-
-                        </>
-                    )
-
+                props.map((props) => {
+                    return <Item
+                        key={props.codigo}
+                        nombre={props.producto}
+                        precio={props.precio}
+                        detalles={props.detalles}
+                    />
                 })
             }
+
         </div>
     )
-
 }
 
 export default ItemList
